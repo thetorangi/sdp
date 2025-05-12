e6e779cbbf9b8f10f8684fa7999cadc4b3786b26
K A R M A ☯️ <torangisb@gmail.com>
2025-05-12 20:50:15 +0530

    Update extended.py

--- extended.py
+++ extended.py
@@ -38,29 +38,51 @@
 #=================
 #=================
 
-from abc import ABC, abstractmethod
+class FrenchLocalizer:
 
-class Animal(ABC):
-    @abstractmethod
-    def speak(self):
-        pass
+    """ it simply returns the french version """
 
-class Dog(Animal):
-    def speak(self):
-        return "Woof!"
+    def __init__(self):
 
-class Cat(Animal):
-    def speak(self):
-        return "Meow!"
+        self.translations = {"car": "voiture", "bike": "bicyclette",
+                             "cycle":"cyclette"}
 
-class AnimalFactory:
-    def create_animal(self, animal_type):
-        if animal_type == "dog":
-            return Dog()
-        elif animal_type == "cat":
-            return Cat()
-        else:
-            raise ValueError("Unknown animal type")
+    def localize(self, msg):
+
+        """change the message using translations"""
+        return self.translations.get(msg, msg)
+
+class SpanishLocalizer:
+    """it simply returns the spanish version"""
+
+    def __init__(self):
+
+        self.translations = {"car": "coche", "bike": "bicicleta",
+                             "cycle":"ciclo"}
+
+    def localize(self, msg):
+
+        """change the message using translations"""
+        return self.translations.get(msg, msg)
+
+class EnglishLocalizer:
+    """Simply return the same message"""
+
+    def localize(self, msg):
+        return msg
+
+# main method to call others
+f = FrenchLocalizer()
+e = EnglishLocalizer()
+s = SpanishLocalizer()
+
+# list of strings
+message = ["car", "bike", "cycle"]
+
+for msg in message:
+    print(f.localize(msg))
+    print(e.localize(msg))
+    print(s.localize(msg))
 
 #=================
 #=================
